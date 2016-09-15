9b809aa8c5d425bd3c3d96556bcc369a4a08a073
Ashwanth Kumar <ashwanthkumar@googlemail.com>
2016-09-16 03:30:16 +0530

    Disabling the os.Hostname == task.Hostname check

--- task-manager.go
+++ task-manager.go
@@ -85,44 +85,42 @@ func (t *TaskManager) run() {
 				}
 			}
 		case task := <-t.InputTasksChannel:
-			if task.Hostname == hostname {
-				// println("Got task for addition.. do what needs to be done")
-				// fmt.Printf("%v\n", task)
-				t.TasksMutex.Lock()
-				_, present := t.KnownTasks[task.TaskID]
-				if !present {
-					log.Printf("TaskID %s is not monitored, sending it to LogManager", task.TaskID)
-					slaveState, _ := t.Client.SlaveState(fmt.Sprintf("http://%s:%d/state.json", hostname, t.SlavePort))
-					// fmt.Printf("%v\n", slaveState)
-					executor := slaveState.FindExecutor(task.TaskID)
-					if executor != nil {
-						logFiles := strings.Split(maps.GetString(task.Labels, LogFilesToMonitor, "stdout"), ",")
-						t.KnownTasks[task.TaskID] = time.Now()
-						for _, file := range logFiles {
-							taskInfo := TaskInfo{
-								App:      task.App,
-								Hostname: task.Hostname,
-								Labels:   task.Labels,
-								TaskID:   task.TaskID,
-								CWD:      executor.Directory,
-								FileName: file,
-							}
-							// fmt.Printf("%v\n", taskInfo)
-							t.AddLogs <- taskInfo
+			// TODO hostname check is an optimisation to avoid HTTP calls to mesos slave
+			// currently in our setup hostnames a little messed up, so disabling this chck for now
+			// if task.Hostname == hostname {
+			// println("Got task for addition.. do what needs to be done")
+			// fmt.Printf("%v\n", task)
+			t.TasksMutex.Lock()
+			_, present := t.KnownTasks[task.TaskID]
+			if !present {
+				log.Printf("TaskID %s is not monitored, sending it to LogManager", task.TaskID)
+				slaveState, _ := t.Client.SlaveState(fmt.Sprintf("http://%s:%d/state.json", hostname, t.SlavePort))
+				// fmt.Printf("%v\n", slaveState)
+				executor := slaveState.FindExecutor(task.TaskID)
+				if executor != nil {
+					logFiles := strings.Split(maps.GetString(task.Labels, LogFilesToMonitor, "stdout"), ",")
+					t.KnownTasks[task.TaskID] = time.Now()
+					for _, file := range logFiles {
+						taskInfo := TaskInfo{
+							App:      task.App,
+							Hostname: task.Hostname,
+							Labels:   task.Labels,
+							TaskID:   task.TaskID,
+							CWD:      executor.Directory,
+							FileName: file,
 						}
-					} else {
-						log.Printf("[WARN] Couldn't find the executor that spun up the task %s", task.TaskID)
+						// fmt.Printf("%v\n", taskInfo)
+						t.AddLogs <- taskInfo
 					}
 				} else {
-					// Already present - update the clock
-					t.KnownTasks[task.TaskID] = time.Now()
+					log.Printf("[WARN] Couldn't find the executor that spun up the task %s", task.TaskID)
 				}
-				t.TasksMutex.Unlock()
+			} else {
+				// Already present - update the clock
+				t.KnownTasks[task.TaskID] = time.Now()
 			}
-			// 1. Check if the task is running on our machine
-			// 2. Check if we already know the task
-			// 3. If yes, just update the KnownTasks map
-			// 4. Else, construct a TaskInfo object and send it to Log Manager and update the KnownTasks map
+			t.TasksMutex.Unlock()
+			// }
 		case <-t.stopChannel:
 			running = false
 		}
